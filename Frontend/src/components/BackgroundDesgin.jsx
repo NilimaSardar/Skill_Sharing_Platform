2e08ac23c955289d0c12cdf1cd6501da16899e25
Nilima Sardar <snilima181@gmail.com>
2025-08-26 11:29:27 +0545

    made BackgroundDesign component and seted on login.jsx

---
+++ Frontend/src/components/BackgroundDesgin.jsx
@@ -0,0 +1,36 @@
+import React from 'react'
+
+const BackgroundDesgin = () => {
+  return (
+    <div>
+              {/* Container for the signup form */}
+      {/* <div className="bg-white p-8 rounded-3xl shadow-xl w-full max-w-md relative overflow-hidden"> */}
+        {/* Background decorative shapes to top-right corner*/}
+        {/* <div className="absolute top-[-356px] left-[114px] w-[635px] h-[635px] bg-[#F8F9FF] rounded-full"></div> */}
+        {/* <div className="absolute top-[-171px] left-[23px] w-[496px] h-[496px] border-[3px] border-[#F8F9FF] rounded-full"></div> */}
+
+        {/* Background decorative shapes to bottom-left corner*/}
+        {/* <div className="absolute top-[450px] right-[306px] w-[372px] h-[372px] border-[2px] border-[#F1F4FF]"></div> */}
+        {/* <div className="absolute top-[450px] right-[306px] w-[372px] h-[372px] border-[2px] border-[#F1F4FF] transform rotate-[27.09deg]"></div> */}
+
+
+        {/* Background decorative shapes to top-right corner*/}
+        {/* <div className="absolute top-[-53%] md:top-[-45%] left-[30%] md:left-[73%] w-[635px] h-[635px] md:w-[600px] md:h-[600px] bg-[#F8F9FF] rounded-full"></div> */}
+        {/* <div className="absolute top-[-26%] md:top-[-28%] left-[6%] md:left-[66%] w-[496px] h-[496px] md:w-[550px] md:h-[550px] border-[3px] border-[#F8F9FF] rounded-full"></div> */}
+
+        {/* Background decorative shapes to bottom-left corner*/}
+        {/* <div className="absolute top-[67.5%] md:top-[65%] right-[80%] md:right-[87%] w-[372px] h-[372px] border-[2px] border-[#F1F4FF]"></div> */}
+        {/* <div className="absolute top-[67.5%] md:top-[65%] right-[80%] md:right-[87%] w-[372px] h-[372px] border-[2px] border-[#F1F4FF] transform rotate-[27.09deg] md:rotate-[35deg]"></div> */}
+
+        {/* Background decorative shapes to top-right corner*/}
+        <div className="absolute top-[-53%] sm:top:[-50%] md:top-[-45%] left-[30%] sm:left-[50%] md:left-[73%] w-[635px] h-[635px] md:w-[600px] md:h-[600px] bg-[#F8F9FF] rounded-full transition-all duration-700 ease-in-out"></div>
+        <div className="absolute top-[-26%] sm:top-[-24%] md:top-[-28%] left-[6%] sm:left-[45%] md:left-[66%] w-[496px] h-[496px] md:w-[550px] md:h-[550px] border-[3px] border-[#F8F9FF] rounded-full transition-all duration-700 ease-in-out"></div>
+
+        {/* Background decorative shapes to bottom-left corner*/}
+        <div className="absolute top-[67.5%] md:top-[65%] right-[80%] md:right-[87%] w-[372px] h-[372px] border-[2px] border-[#F1F4FF] transition-all duration-700 ease-in-out"></div>
+        <div className="absolute top-[67.5%] md:top-[65%] right-[80%] md:right-[87%] w-[372px] h-[372px] border-[2px] border-[#F1F4FF] transform rotate-[27.09deg] md:rotate-[35deg] transition-all duration-700 ease-in-out"></div>
+    </div>
+  )
+}
+
+export default BackgroundDesgin
---
+++ Frontend/src/pages/Login.jsx
@@ -0,0 +1,14 @@
+import React, { useState } from 'react';
+import BackgroundDesgin from '../components/BackgroundDesgin';
+
+export default function App() {
+
+
+  return (
+    <div className="min-h-screen bg-white flex items-center justify-center p-8 font-inter relative overflow-hidden">
+
+      <BackgroundDesgin/>
+
+    </div>
+  );
+}
