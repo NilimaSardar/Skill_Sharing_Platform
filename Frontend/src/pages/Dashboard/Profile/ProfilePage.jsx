145ead0e504d2d8e9d25dad60848480bef024242
Nilima Sardar <snilima181@gmail.com>
2025-11-10 15:32:55 +0545

    working on profile page

--- Frontend/src/pages/Dashboard/Profile/ProfilePage.jsx
+++ Frontend/src/pages/Dashboard/Profile/ProfilePage.jsx
@@ -10,66 +10,75 @@ const ProfilePage = () => {
         <div onClick={() => navigate(-1)} className="w-[35px] h-[35px] bg-icon-bg-hover flex items-center justify-center rounded-full cursor-pointer">
             <img src="../../images/BackArrow.svg" alt="notification bell" className='w-[25px] h-[25px]'/>
         </div>
-        <h3 className='font-serif w-full text-center text-xl'>Message</h3>
+        <h3 className='font-serif w-full text-center text-xl'>Profile</h3>
+        <NavLink className="w-[35px] h-[35px] flex items-center justify-center rounded-full cursor-pointer">
+            <img src="../../images/Settings.svg" alt="setting" className='w-[25px] h-[25px]'/>
+        </NavLink>
     </div>
 
-    {/* active profiles */}
-    <div className="flex gap-4 overflow-x-auto py-2 hide-scrollbar">
-    {["Alice", "Bob", "Charlie", "Diana", "Eve", "Frank"].map((user, idx) => (
-        <div key={idx} className="flex flex-col items-center">
-        <div className="relative w-[50px] h-[50px] flex items-center justify-center cursor-pointer">
-            <img
-            src="../../profile/Nilima.jpeg"
-            alt={user}
-            className="w-[45px] h-[45px] rounded-full object-cover border-2 border-white shadow"
-            />
-            {/* Active green dot */}
-            <span className="absolute bottom-1 right-1 w-[12px] h-[12px] bg-green-500 border-2 border-white rounded-full"></span>
+    {/* profile */}
+    <div className="flex flex-col justify-center">
+        <div className="flex flex-col items-center">
+            <div className="relative w-[85px] h-[85px] flex items-center justify-center cursor-pointer">
+                <img
+                src="../../profile/Nilima.jpeg"
+                alt=""
+                className="w-[80px] h-[80px] rounded-full object-cover border-2 border-primary-light shadow"
+                />
+                {/* Edit button */}
+                <img src="../../images/Edit.svg" alt="" className="absolute bottom-1 right-1 border-1 border-white rounded-full"/>
+            </div>
+            <p className="text-[18px] text-center font-medium mt-2">Nilima Sardar</p>
         </div>
-        <p className="text-[14px] text-center font-medium mt-1">{user}</p>
-        </div>
-    ))}
-    </div>
 
-    {/* Search bar */}
-    <div className="my-3">
-        <div className="relative w-full">
-            <input
-            type="text"
-            placeholder="Search"
-            className="w-full pr-10 pl-4 py-2 text-[16px] text-gray-800  font-medium border border-border bg-search-bg rounded-lg focus:ring-2 focus:ring-indigo-500 focus:outline-none placeholder-gray-400"
-            />
-            <img
-            src="../../images/Search_Icon.svg"
-            alt="Search"
-            className="absolute right-3 top-1/2 transform -translate-y-1/2 w-5 h-5 text-gray-500 pointer-events-none"
-            />
+        <div className='flex justify-between my-4'>
+          <div className='flex flex-col items-center border-r border-border pr-6'>
+            <h4 className='text-[20px] text-text font-black tracking-wide'>54</h4>
+            <p className='text-[#737373] text-[14px]'>Connected</p>
+          </div>
+          <div className='flex flex-col items-center border-r border-border pr-6'>
+            <h4 className='text-[20px] text-text font-black tracking-wide'>07</h4>
+            <p className='text-[#737373] text-[14px]'>Conducted</p>
+          </div>
+          <div className='flex flex-col items-center'>
+            <h4 className='text-[20px] text-text font-black tracking-wide'>03</h4>
+            <p className='text-[#737373] text-[14px]'>Attendend</p>
+          </div>
+        </div>
+
+        {/* About */}
+        <div>
+          <h4>About Me</h4>
+          <p className="text-[#737373] text-[14px] text-justify">I'm a seasoned creative expert dedicated to sharing the proven blueprint for achieving 
+            top-tier Web Development and UI/UX results. My focus is entirely on practical, real-world
+            knowledge and actionable skills, not abstract theory. Join me, and let's work side-by-side
+            to master the processes needed to create dynamic... <span className='text-primary font-medium'>Read More</span></p>
         </div>
     </div>
 
     {/* Search catogoy */}
     <div className="flex w-full gap-4 text-center text-[16px] font-medium text-text">
-    <NavLink
-      to="individual"
-      className={({ isActive }) =>
-        `py-[10px] px-[18px] w-1/2 rounded-lg ${
-          isActive ? "bg-primary-light text-text border border-border" : "text-text hover:text-primary border border-border"
-        }`
-      }
-    >
-      Individual
-    </NavLink>
+      <NavLink
+        to="individual"
+        className={({ isActive }) =>
+          `py-[10px] px-[18px] w-1/2 rounded-lg ${
+            isActive ? "bg-primary-light text-text border border-border" : "text-text hover:text-primary border border-border"
+          }`
+        }
+      >
+        My Skills
+      </NavLink>
 
-    <NavLink
-      to="community"
-      className={({ isActive }) =>
-        `py-[10px] px-[18px] w-1/2 rounded-lg ${
-          isActive ? "bg-primary-light text-text border border-border" : "text-text hover:text-primary border border-border"
-        }`
-      }
-    >
-      Community
-    </NavLink>
+      <NavLink
+        to="community"
+        className={({ isActive }) =>
+          `py-[10px] px-[18px] w-1/2 rounded-lg ${
+            isActive ? "bg-primary-light text-text border border-border" : "text-text hover:text-primary border border-border"
+          }`
+        }
+      >
+        Trade History
+      </NavLink>
     </div>
 
   {/* Content area */}
